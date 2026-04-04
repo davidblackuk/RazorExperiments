f2e324dde2cbe145db603ed9120e580b28ace0f8
David Black <david_black_uk@yahoo.com>
2026-04-04 13:37:03 +0100

    added new razor project to the solution

---
+++ FirstApp/FirstApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FirstApp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
