ba2a79ba03342af065149c1f3304e112c53bb908
David Black <david_black_uk@yahoo.com>
2026-04-05 09:07:16 +0100

    made login be required for all pages but the main page

--- FirstApp/FirstApp/Program.cs
+++ FirstApp/FirstApp/Program.cs
@@ -12,7 +12,11 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/");
+    options.Conventions.AllowAnonymousToPage("/Index");
+});
 
 var app = builder.Build();
 
