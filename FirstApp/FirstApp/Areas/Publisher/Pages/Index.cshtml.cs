a6e0c4c18e3edd16e409a2731f1cfd9033a2087c
David Black <david_black_uk@yahoo.com>
2026-05-04 13:12:28 +0100

    added two new areas

---
+++ FirstApp/FirstApp/Areas/Publisher/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FirstApp.Areas.Publisher.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
